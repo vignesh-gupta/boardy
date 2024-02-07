ac336a21780862757603761b7f7ee8c3d602f511
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-02-08 02:26:46 +0530

    fix build issue

--- src/app/board/[boardId]/_components/layers/note.tsx
+++ src/app/board/[boardId]/_components/layers/note.tsx
@@ -3,7 +3,6 @@ import { NoteLayer } from "@/types/canvas";
 import { useMutation } from "@root/liveblocks.config";
 import { Kalam } from "next/font/google";
 import React from "react";
-import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
 
 const font = Kalam({
   subsets: ["latin"],
@@ -36,7 +35,11 @@ const Note = ({ id, layer, onPointerDown, selectionColor }: NoteProps) => {
     layer.get(id)?.set("value", newValue);
   }, []);
 
-  const handleContentChange = (e: ContentEditableEvent) => {
+  const handleContentChange = (
+    e: React.SyntheticEvent<any, Event> & {
+      target: { value: string };
+    }
+  ) => {
     updateValue(e.target.value);
   };
 
