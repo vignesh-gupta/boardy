128285362a7aeca2024798fa5bbcfb7b07f7e334
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-06-05 09:32:40 +0530

    fix: `liveblock` upgrade issue by updaing `liveblock config`

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -1,16 +1,18 @@
 import { Color, Layer } from "@/types/canvas";
 import {
-  createClient,
   LiveList,
   LiveMap,
-  LiveObject
+  LiveObject,
+  createClient
 } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 
 const client = createClient({
   // publicApiKey: "",
   authEndpoint: "/api/liveblocks-auth",
-  throttle: 16
+  throttle: 16,
+  resolveUsers: async ({ userIds }) => [],
+  resolveMentionSuggestions: async ({ text, roomId }) => []
 });
 
 // Presence represents the properties that exist on every user in the Room
@@ -74,9 +76,6 @@ export const {
     useEventListener,
     useErrorListener,
     useStorage,
-    useObject,
-    useMap,
-    useList,
     useBatch,
     useHistory,
     useUndo,
@@ -97,43 +96,5 @@ export const {
     useRemoveReaction
   }
 } = createRoomContext<Presence, Storage, UserMeta, RoomEvent, ThreadMetadata>(
-  client,
-  {
-    async resolveUsers({ userIds }) {
-      // Used only for Comments. Return a list of user information retrieved
-      // from `userIds`. This info is used in comments, mentions etc.
-
-      // const usersData = await __fetchUsersFromDB__(userIds);
-      //
-      // return usersData.map((userData) => ({
-      //   name: userData.name,
-      //   avatar: userData.avatar.src,
-      // }));
-
-      return [];
-    },
-    async resolveMentionSuggestions({ text, roomId }) {
-      // Used only for Comments. Return a list of userIds that match `text`.
-      // These userIds are used to create a mention list when typing in the
-      // composer.
-      //
-      // For example when you type "@jo", `text` will be `"jo"`, and
-      // you should to return an array with John and Joanna's userIds:
-      // ["john@example.com", "joanna@example.com"]
-
-      // const userIds = await __fetchAllUserIdsFromDB__(roomId);
-      //
-      // Return all userIds if no `text`
-      // if (!text) {
-      //   return userIds;
-      // }
-      //
-      // Otherwise, filter userIds for the search `text` and return
-      // return userIds.filter((userId) =>
-      //   userId.toLowerCase().includes(text.toLowerCase())
-      // );
-
-      return [];
-    }
-  }
+  client
 );
